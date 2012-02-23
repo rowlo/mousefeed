805df3e261b7e7e7626356db41255831718ec39d
Robert Wloch <robert@rowlo.de>
2012-02-23 01:20:38 +0100

    Workaround for avoiding ArrayIndexOutOfBoundsException due to bad buggy code in BindingService class of Eclipse 4. However, although the exception is gone, the mousefeed popup still doesn't show up in Eclipse 4 -> needs further work

--- com.mousefeed/src/com/mousefeed/eclipse/ActionActionDescGenerator.java
+++ com.mousefeed/src/com/mousefeed/eclipse/ActionActionDescGenerator.java
@@ -28,8 +28,8 @@ import org.apache.commons.lang.StringUtils;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.jface.action.ExternalActionManager;
-import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.ExternalActionManager.ICallback;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.bindings.Binding;
 import org.eclipse.jface.bindings.TriggerSequence;
 import org.eclipse.jface.bindings.keys.KeySequence;
@@ -39,31 +39,31 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.RetargetAction;
 import org.eclipse.ui.activities.IActivityManager;
+import org.eclipse.ui.internal.keys.BindingService;
 import org.eclipse.ui.keys.IBindingService;
 
 //COUPLING:OFF
 /**
- * Generates {@link ActionDescImpl} from {@link IAction}.
- * Pure strategy.
- * The logic was developed by trying different actions, making sure all the
+ * Generates {@link ActionDescImpl} from {@link IAction}. Pure strategy. The
+ * logic was developed by trying different actions, making sure all the
  * ActionDesc components are correctly generated.
- *
+ * 
  * @author Andriy Palamarchuk
  * @author Robert Wloch
  */
+@SuppressWarnings("restriction")
 public class ActionActionDescGenerator {
 
     /**
      * Stores the generated action description.
      */
-    private ActionDescImpl actionDesc; 
-    
+    private ActionDescImpl actionDesc;
+
     /**
      * Searches for an action inside of
      * <code>org.eclipse.ui.actions.TextActionHandler</code> utility actions.
      */
-    private final TextActionHandlerActionLocator actionSearcher =
-            new TextActionHandlerActionLocator();
+    private final TextActionHandlerActionLocator actionSearcher = new TextActionHandlerActionLocator();
 
     /**
      * The binding service for the associated workbench.
@@ -79,19 +79,19 @@ public class ActionActionDescGenerator {
      * Creates new finder.
      */
     public ActionActionDescGenerator() {
-        bindingService =
-            (IBindingService) getWorkbench().getAdapter(IBindingService.class);
-        activityManager =
-            getWorkbench().getActivitySupport().getActivityManager();
+        bindingService = (IBindingService) getWorkbench().getAdapter(
+                IBindingService.class);
+        activityManager = getWorkbench().getActivitySupport()
+                .getActivityManager();
     }
-    
 
     /**
      * Generates action description from the action.
-     * @param action the action to generate description for.
-     * Not <code>null</code>.
-     * @return the action description for the provided action.
-     * Never <code>null</code>.
+     * 
+     * @param action
+     *            the action to generate description for. Not <code>null</code>.
+     * @return the action description for the provided action. Never
+     *         <code>null</code>.
      */
     public AbstractActionDesc generate(final IAction action) {
         notNull(action);
@@ -112,11 +112,12 @@ public class ActionActionDescGenerator {
         return actionDesc;
     }
 
-    //clear, simple structure, leave extra returns
+    // clear, simple structure, leave extra returns
     /**
      * Scans action for data to populate action description.
-     * @param action the action to search accelerator for.
-     * Not <code>null</code>.
+     * 
+     * @param action
+     *            the action to search accelerator for. Not <code>null</code>.
      */
     public void extractActionData(final IAction action) {
         notNull(action);
@@ -134,10 +135,11 @@ public class ActionActionDescGenerator {
         fromActionBinding(action);
     }
 
-
     /**
      * Populates {@link #actionDesc} from the action definition id.
-     * @param actionDefinitionId. <code>null</code> if not defined.
+     * 
+     * @param actionDefinitionId
+     *            . <code>null</code> if not defined.
      */
     private void fromActionDefinition(final String definitionId) {
         if (definitionId == null) {
@@ -150,10 +152,11 @@ public class ActionActionDescGenerator {
         }
     }
 
-
     /**
      * Populates {@link #actionDesc} from the action accelerator.
-     * @param accelerator. 0 if not defined.
+     * 
+     * @param accelerator
+     *            . 0 if not defined.
      */
     private void fromActionAccelerator(final int accelerator) {
         if (accelerator != 0) {
@@ -163,39 +166,42 @@ public class ActionActionDescGenerator {
 
     /**
      * Finds keyboard shortcut for the action definition id.
-     * @param actionDefinitionId the action definition id to search accelerator
-     * for. If blank, the method returns <code>null</code>
-     * @return A human-readable string representation of the accelerator.
-     * Is <code>null</code> if can't be found.
+     * 
+     * @param actionDefinitionId
+     *            the action definition id to search accelerator for. If blank,
+     *            the method returns <code>null</code>
+     * @return A human-readable string representation of the accelerator. Is
+     *         <code>null</code> if can't be found.
      */
     private String findAcceleratorForActionDefinition(final String definitionId) {
         if (StringUtils.isBlank(definitionId)) {
             return null;
         }
-        final ICallback callback =
-                ExternalActionManager.getInstance().getCallback();
+        final ICallback callback = ExternalActionManager.getInstance()
+                .getCallback();
         return callback.getAcceleratorText(definitionId);
     }
 
     /**
      * Scans current bindings, returns a trigger sequence, associated with this
-     * action. Uses heuristic to find an association.
-     * Assumes that if a binding action and the provided action are the same,
-     * if they have same class.
-     * This logic can potentially fail if an action class is used for
-     * different actions.
-     * @param action the action to search trigger sequence for.
-     * Returns <code>null</code> if the action is <code>null</code>.
-     * @return the accelerator from the trigger sequence associated
-     * with the action or <code>null</code> if such sequence was not found.
+     * action. Uses heuristic to find an association. Assumes that if a binding
+     * action and the provided action are the same, if they have same class.
+     * This logic can potentially fail if an action class is used for different
+     * actions.
+     * 
+     * @param action
+     *            the action to search trigger sequence for. Returns
+     *            <code>null</code> if the action is <code>null</code>.
+     * @return the accelerator from the trigger sequence associated with the
+     *         action or <code>null</code> if such sequence was not found.
      */
     private String fromActionBinding(final IAction action) {
         if (action == null) {
             return null;
         }
         if (action instanceof RetargetAction) {
-            return fromActionBinding(
-                    ((RetargetAction) action).getActionHandler());
+            return fromActionBinding(((RetargetAction) action)
+                    .getActionHandler());
         }
 
         return scanBindings(action);
@@ -203,49 +209,65 @@ public class ActionActionDescGenerator {
 
     /**
      * Scans bindings for the action.
-     * @param action the action to scan bindings for.
-     * Assumed not <code>null</code>.
+     * 
+     * @param action
+     *            the action to scan bindings for. Assumed not <code>null</code>
+     *            .
      * @return the accelerator from the action trigger sequence.
-     * <code>null</code> if the binding was not found.
+     *         <code>null</code> if the binding was not found.
      */
     @SuppressWarnings("rawtypes")
     private String scanBindings(final IAction action) {
-        final Map matches = bindingService.getPartialMatches(
-                KeySequence.getInstance());
-        final Class<? extends IAction> actionClass = action.getClass();
-        for (Object o : matches.keySet()) {
-            final TriggerSequence triggerSequence = (TriggerSequence) o;
-            final Binding binding = (Binding) matches.get(triggerSequence);
-            final Command command =
-                    binding.getParameterizedCommand().getCommand();
-            final IHandler handler = getCommandHandler(command);
-            if (!(handler instanceof ActionHandler)) {
-                continue;
-            }
+        final KeySequence keySequence = KeySequence.getInstance();
+        // In eclipse 4 BindingService#getPartialMatches() doesn't call the
+        // BindingManager any more leading to an ArrayIndexOutOfBoundsException
+        // due to bad code quality. Only for this reason, mousefeed needs to
+        // perform a direct call on the BindingManager now.
+        if (bindingService instanceof BindingService) {
+            final BindingService theBindingService = (BindingService) bindingService;
+            final Map matches = theBindingService.getBindingManager()
+                    .getPartialMatches(keySequence);
 
-            if (isCommandEnabled(command)) {
-                final String accelerator = getFromBindingData(
-                        action, actionClass, triggerSequence, handler);
-                if (accelerator != null) {
-                    return accelerator;
+            final Class<? extends IAction> actionClass = action.getClass();
+            for (Object o : matches.keySet()) {
+                final TriggerSequence triggerSequence = (TriggerSequence) o;
+                final Binding binding = (Binding) matches.get(triggerSequence);
+                final Command command = binding.getParameterizedCommand()
+                        .getCommand();
+                final IHandler handler = getCommandHandler(command);
+                if (!(handler instanceof ActionHandler)) {
+                    continue;
+                }
+
+                if (isCommandEnabled(command)) {
+                    final String accelerator = getFromBindingData(action,
+                            actionClass, triggerSequence, handler);
+                    if (accelerator != null) {
+                        return accelerator;
+                    }
                 }
             }
         }
         return null;
     }
 
-    //RETURNCOUNT:OFF
-    //clear, simple structure, leave extra returns
+    // RETURNCOUNT:OFF
+    // clear, simple structure, leave extra returns
     /**
      * Gets accelerator for the bindings data.
-     * @param action the action. Assumed not null.
-     * @param actionClass effective action class. Assumed not null.
-     * @param triggerSequence the binding trigger sequence. Assumed not null.
-     * @param handler the action handler. Assumed not null. 
+     * 
+     * @param action
+     *            the action. Assumed not null.
+     * @param actionClass
+     *            effective action class. Assumed not null.
+     * @param triggerSequence
+     *            the binding trigger sequence. Assumed not null.
+     * @param handler
+     *            the action handler. Assumed not null.
      * @return the accelerator if found, <code>null</code> otherwise.
      */
-    private String getFromBindingData(
-            final IAction action, final Class<? extends IAction> actionClass,
+    private String getFromBindingData(final IAction action,
+            final Class<? extends IAction> actionClass,
             final TriggerSequence triggerSequence, final IHandler handler) {
         final ActionHandler actionHandler = (ActionHandler) handler;
         final IAction boundAction = actionHandler.getAction();
@@ -258,8 +280,8 @@ public class ActionActionDescGenerator {
         if (!(boundAction instanceof RetargetAction)) {
             return null;
         }
-        final IAction searchTarget =
-                ((RetargetAction) boundAction).getActionHandler(); 
+        final IAction searchTarget = ((RetargetAction) boundAction)
+                .getActionHandler();
         if (searchTarget == null) {
             return null;
         }
@@ -267,23 +289,26 @@ public class ActionActionDescGenerator {
             return triggerSequence.toString();
         }
         if (actionSearcher.isSearchable(searchTarget)) {
-            final String id =
-                    actionSearcher.findActionDefinitionId(action, searchTarget);
+            final String id = actionSearcher.findActionDefinitionId(action,
+                    searchTarget);
             return findAcceleratorForActionDefinition(id);
         }
         return null;
     }
-    //RETURNCOUNT:ON
+
+    // RETURNCOUNT:ON
 
     /**
      * Converts the provided key combination code to accelerator.
-     * @param accelerator The accelerator to convert; should be a valid SWT
-     * accelerator value.
+     * 
+     * @param accelerator
+     *            The accelerator to convert; should be a valid SWT accelerator
+     *            value.
      * @return The equivalent key stroke; never <code>null</code>.
      */
     private String keyToStr(final int accelerator) {
-        return
-            SWTKeySupport.convertAcceleratorToKeyStroke(accelerator).format();
+        return SWTKeySupport.convertAcceleratorToKeyStroke(accelerator)
+                .format();
     }
 
     /**
@@ -295,10 +320,12 @@ public class ActionActionDescGenerator {
 
     /**
      * Retrieves command handler from a command.
-     * @param command the command to retrieve the handler from.
-     * Not <code>null</code>.
-     * @return the handler. Returns <code>null</code>,
-     * if can't retrieve a handler. 
+     * 
+     * @param command
+     *            the command to retrieve the handler from. Not
+     *            <code>null</code>.
+     * @return the handler. Returns <code>null</code>, if can't retrieve a
+     *         handler.
      */
     private IHandler getCommandHandler(final Command command) {
         try {
@@ -321,12 +348,14 @@ public class ActionActionDescGenerator {
     }
 
     /**
-     * Returns <code>true</code> if the command is defined and is enabled. 
-     * @param command the command to check. Not <code>null</code>.
+     * Returns <code>true</code> if the command is defined and is enabled.
+     * 
+     * @param command
+     *            the command to check. Not <code>null</code>.
      */
     private boolean isCommandEnabled(final Command command) {
         return command.isDefined()
                 && activityManager.getIdentifier(command.getId()).isEnabled();
     }
 }
-//COUPLING:ON
+// COUPLING:ON
